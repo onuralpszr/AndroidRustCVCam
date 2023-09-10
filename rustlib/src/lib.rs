1ad786da308aebf59e2992420b0b81c71b8195f8
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-09-10 19:40:30 +0300

    feat: ✨ crate ndk added for access Android NDK functions and simple bitmap width read function on rust side added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -1,18 +1,41 @@
-#![cfg(target_os="android")]
 #![allow(non_snake_case)]
 
+
 use jni::JNIEnv;
-use jni::objects::{JClass, JString};
+use jni::sys::jint;
+use jni::objects::{JClass, JString, JObject};
+use ndk::bitmap::{AndroidBitmap, AndroidBitmapInfo};
+use log::info;
+use opencv::{highgui, imgcodecs, imgproc, prelude::*};
+use opencv::core::Mat;
+use opencv::core::CV_VERSION;
 
 
-// https://doc.rust-lang.org/reference/items/external-blocks.html#abi
 #[no_mangle]
 pub extern "system" fn Java_com_os_androidrustcvcam_MainActivityKt_opencvVersion<'local>(
-    mut env: JNIEnv<'local>,
+     env: JNIEnv<'local>,
     _class: JClass<'local>,
 ) -> JString<'local> {
 
     // Get opencv version
-    let version = opencv::core::CV_VERSION;
+    let version = CV_VERSION;
     env.new_string(version).unwrap()
 }
+
+
+pub extern "system" fn Java_com_os_androidrustcvcam_MainActivityKt_readBitmapWidth<'local>(
+    env: JNIEnv<'local>,
+    _class: JClass<'local>,
+    mut _bitmap: JObject,
+) -> jint {
+
+    let _res: AndroidBitmap = unsafe { AndroidBitmap::from_jni(
+        env.get_native_interface(), 
+        *_bitmap) };
+
+    let bmp_info: AndroidBitmapInfo = _res.get_info().expect("Failed to get bitmap info");
+    let width: i32 = bmp_info.width() as i32;
+
+    width
+}
+
