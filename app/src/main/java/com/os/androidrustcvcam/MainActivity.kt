1e910730e9ca26fe8acccd2ea95b0b53e0ababf7
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-09-10 19:34:08 +0300

    feat: ✨ newest coil with URL image load and bitmap conversion
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- app/src/main/java/com/os/androidrustcvcam/MainActivity.kt
+++ app/src/main/java/com/os/androidrustcvcam/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.os.androidrustcvcam
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,9 +17,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.os.androidrustcvcam.ui.theme.AndroidRustTemplateTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,39 +36,85 @@ class MainActivity : ComponentActivity() {
             AndroidRustTemplateTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    OpencvVersion(opencvVersion())
+                    OpenCVCompose(opencvVersion());
                 }
             }
         }
     }
 
-    companion object {
-        init {
-            System.loadLibrary("rustlib")
-        }
+    companion object {init {
+        System.loadLibrary("rustlib")
     }
-
+    }
 }
 
 private external fun opencvVersion(): String
 
 @Composable
-fun OpencvVersion(ver: String, modifier: Modifier = Modifier) {
+fun OpenCVCompose(ver: String, modifier: Modifier = Modifier) {
+
     Column(
         modifier = Modifier
             .padding(30.dp)
             .fillMaxWidth()
             .wrapContentSize(Alignment.Center)
     ) {
-
         Text(
-            text = "OpenCV Version: $ver",
-            modifier = modifier
-
+            text = "OpenCV Version: $ver", modifier = modifier
         )
+        DisplayBmp()
+    }
+}
+
+
+@Composable
+fun DisplayBmp(modifier: Modifier = Modifier) {
+
+    var originalBmp = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+    var modifiedBmpCv = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+    val baseImage =
+        "https://images.unsplash.com/photo-1628373383885-4be0bc0172fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80"
+    val basePainter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current).data(baseImage)
+            .size(coil.size.Size.ORIGINAL) // Set the target size to load the image at.
+            .build()
+    )
+
+    val baseImageLoadedState = basePainter.state
+    if (baseImageLoadedState is AsyncImagePainter.State.Success) originalBmp =
+        baseImageLoadedState.result.drawable.toBitmap()
+
+
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+
+    ) {
+
+        // Display bitmap image
+        Image(
+            painter = rememberAsyncImagePainter(originalBmp),
+            contentDescription = "Image",
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f)
+                .clip(MaterialTheme.shapes.medium),
+            contentScale = ContentScale.Crop
+        )
+        Image(
+            painter = rememberAsyncImagePainter(originalBmp),
+            contentDescription = "Image",
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f)
+                .clip(MaterialTheme.shapes.medium),
+            contentScale = ContentScale.Crop
+        )
+
     }
 
 }
@@ -66,6 +123,6 @@ fun OpencvVersion(ver: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidRustTemplateTheme {
-        OpencvVersion("OpenCV Version")
+        OpenCVCompose("OpenCV Version")
     }
 }
